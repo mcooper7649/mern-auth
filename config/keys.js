20296bc78f74855eaa2807cf7618718e630918c6
Michael Cooper <mcooper7649@gmail.com>
2021-09-19 23:11:06 -0400

    backend setup

---
+++ config/keys.js
@@ -0,0 +1,4 @@
+module.exports = {
+    mongoURI: "mongodb+srv://admin-mike:9NMCTko4NoWXtfcw@cluster0.ewevp.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
+    secretOrKey: "secret"
+  };
---
+++ readme.md
@@ -0,0 +1,4 @@
+[link-to-tutorial](https://blog.bitsrc.io/build-a-login-auth-app-with-mern-stack-part-1-c405048e3669)
+
+1. installed dependencies
+2. # mern-auth
